4effea97b9cb57047a7604f51403b8b794b3e29b
yeaImAlex <96880676+yeaImAlex@users.noreply.github.com>
2024-09-20 23:13:35 +0800

    Create keyboardM.py

---
+++ keyboardM.py
@@ -0,0 +1,37 @@
+import pygame
+
+class Keyboard1:
+    def __init__(self):
+        pygame.init()
+        self.win = pygame.display.set_mode((100,100))
+
+    def getKey(self, keyName):
+        ans = False
+        for eve in pygame.event.get(): pass
+        keyInput = pygame.key.get_pressed()
+        myKey = getattr(pygame, 'K_{}'.format(keyName))
+        if keyInput[myKey]:
+            ans = True
+        pygame.display.update()
+
+        return ans
+
+    def main(self):
+        if self.getKey('a'):
+            print('Left')
+        if self.getKey('d'):
+            print('Right')
+        if self.getKey('w'):
+            print('Forward')
+        if self.getKey('s'):
+            print('Backward')
+        if self.getKey('e'):
+            print('Right forward')
+        if self.getKey('q'):
+            print('Left forward')
+
+if __name__ == '__main__':
+        keyboard = Keyboard1()
+
+        while True:
+            keyboard.main()
